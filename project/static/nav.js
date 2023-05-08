e2262e351da725f4c6088a63283238473a7bed9b
ShailMurtaza <shailmurtaza@gmail.com>
2023-05-08 18:15:58 +0500

    Wait for z-index to set before transateY use transition only for css transform property

--- project/static/nav.js
+++ project/static/nav.js
@@ -4,17 +4,22 @@ var nav = document.getElementById("nav")
 var search_input = document.getElementById("search_input")
 
 function show_search() {
-    search.style.visibility = "visible"
     search.style.transform = "translateY(0)"
+    // setTimeout(()=> {search.style.zIndex = 0}, 1000)
     search.style.zIndex = 0
     nav.style.boxShadow = "none"
     setTimeout(()=> {search_input.focus()}, 700)
 }
 
-function hide_search() {
-    search.style.transform = "translateY(-70px)"
-    search.style.zIndex = -1;
-    search.style.visibility = "hidden"
 
-    nav.style.boxShadow = "0 5px 15px rgba(92, 92, 92, 0.7)"
+function hide_search() {
+    const zIndexPromise = new Promise((resolve) => {
+        search.style.zIndex = '-1';
+        resolve();
+    });
+    zIndexPromise.then(function () {
+        search.style.transform = "translateY(-70px)"
+        // search.style.visibility = "hidden"
+        nav.style.boxShadow = "0 5px 15px rgba(92, 92, 92, 0.7)"
+    })
 }
