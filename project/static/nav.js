3cbe87bf867ad74bce3e718130e9ddd10683e973
ShailMurtaza <shailmurtaza@gmail.com>
2023-05-08 17:42:57 +0500

    Done NavBar and Search Bar in header section Created separated shadows for search bar and navbar hide shadow of nav when open search bar and show it when search bar will be closed

--- project/static/nav.js
+++ project/static/nav.js
@@ -1,6 +1,20 @@
 var search = document.getElementById("search")
+var header = document.getElementById("header")
+var nav = document.getElementById("nav")
+var search_input = document.getElementById("search_input")
 
-function show_search(){
-    alert(1)
-    search.style.display = "flex";
+function show_search() {
+    search.style.visibility = "visible"
+    search.style.transform = "translateY(0)"
+    search.style.zIndex = 0
+    nav.style.boxShadow = "none"
+    setTimeout(()=> {search_input.focus()}, 700)
+}
+
+function hide_search() {
+    search.style.transform = "translateY(-70px)"
+    search.style.zIndex = -1;
+    search.style.visibility = "hidden"
+
+    nav.style.boxShadow = "0 5px 15px rgba(92, 92, 92, 0.7)"
 }
