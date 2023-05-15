21a3aa84ae03f36c10dbf8eaedc0051eab62dfaf
ShailMurtaza <shailmurtaza@gmail.com>
2023-05-15 20:12:41 +0500

    Created responsive animated navbar

--- project/static/header.js
+++ project/static/header.js
@@ -1,7 +1,14 @@
 var search = document.getElementById("search")
 var header = document.getElementById("header")
 var nav = document.getElementById("nav")
+var nav2 = document.getElementById("nav2")
 var search_input = document.getElementById("search_input")
+var container = document.getElementById("container")
+
+function after_load() {
+    container.style.top = nav2.clientHeight + "px"
+}
+
 
 function show_search() {
     search.style.transform = "translateY(0)"
@@ -25,5 +32,19 @@ function hide_search() {
 }
 
 function toggleNav() {
-    alert(1)
+    // Navbar is closed if top is not 0px
+    if (nav.style.top != "0px") {
+        // Show NAV
+        nav.style.top = "0px"
+        container.style.top = "0px"
+        setTimeout(()=>{nav.style.position = "relative"}, 510)
+    }
+    else {
+        // Hide NAV
+        nav.style.top = "-300px"
+        nav.style.position = "absolute"
+        container.style.top = nav2.clientHeight + "px"
+        // Also hide search just in case if it might be opened. It just look ugly when nav is closed and search is still showing
+        hide_search()
+    }
 }
