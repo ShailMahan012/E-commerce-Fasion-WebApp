718ea4800a4fa37866244a9acecb4c80143f7409
Shail Mahan <shailmahan012@gmail.com>
2023-05-16 00:44:27 -0400

    Done with rotation animation of toggle button

--- project/static/header.js
+++ project/static/header.js
@@ -1,9 +1,14 @@
-var search = document.getElementById("search")
-var header = document.getElementById("header")
-var nav = document.getElementById("nav")
-var nav2 = document.getElementById("nav2")
-var search_input = document.getElementById("search_input")
-var container = document.getElementById("container")
+function get(id) {
+    return document.getElementById(id)
+}
+
+var search = get("search")
+var header = get("header")
+var nav = get("nav")
+var nav2 = get("nav2")
+var search_input = get("search_input")
+var container = get("container")
+var btn_toggle_nav = get("btn-toggle-nav")
 
 function after_load() {
     container.style.top = nav2.clientHeight + "px"
@@ -34,12 +39,18 @@ function hide_search() {
 function toggleNav() {
     // Navbar is closed if top is not 0px
     if (nav.style.top != "0px") {
+        btn_toggle_nav.classList.add("nav-show-rotate")
+        setTimeout(()=>{btn_toggle_nav.classList.remove("nav-show-rotate")}, 1020)
+
         // Show NAV
         nav.style.top = "0px"
         container.style.top = "0px"
         setTimeout(()=>{nav.style.position = "relative"}, 510)
     }
     else {
+        btn_toggle_nav.classList.add("nav-hide-rotate")
+        setTimeout(()=>{btn_toggle_nav.classList.remove("nav-hide-rotate")}, 1020)
+
         // Also hide search just in case if it might be opened. It just look ugly when nav is closed and search is still showing
         hide_search()
         // Hide NAV
