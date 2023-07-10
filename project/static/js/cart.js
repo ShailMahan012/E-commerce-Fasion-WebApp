aec51b24105caaff6b070aa35752f09685a70705
ShailMurtaza <shailmurtaza@gmail.com>
2023-07-10 21:23:39 +0500

    add_to_cart: check for product ID if it already exists in localStorage

--- project/static/js/cart.js
+++ project/static/js/cart.js
@@ -3,9 +3,20 @@ if (!localStorage.getItem("products")) {
     localStorage.setItem("products", '[]') // make products key with empty array
 }
 
+
 function add_to_cart(id, quantity) {
     var products = JSON.parse(localStorage.getItem("products"))
-    products.push({id:id, quantity:quantity})
+    if (find_product_id(products, id) === false)
+        products.push({id:id, quantity:quantity})
     console.log(products)
     localStorage.setItem("products", JSON.stringify(products))
+}
+
+
+function find_product_id(products, id) {
+    for (let i=0;i<products.length;i++) {
+        if (products[i].id === id)
+            return true
+    }
+    return false
 }
