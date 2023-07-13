7b366a57f7cc24338eaa82f61325a42026709758
Shail Mahan <shailmahan012@gmail.com>
2023-07-13 03:07:48 -0400

    delete_from_cart: function to remove product from JSON

--- project/static/js/cart.js
+++ project/static/js/cart.js
@@ -8,8 +8,21 @@ function add_to_cart(id, quantity) {
     var products = JSON.parse(localStorage.getItem("products"))
     if (find_product_id(products, id) === false)
         products.push({id:id, quantity:quantity})
-    console.log(products)
     localStorage.setItem("products", JSON.stringify(products))
+    console.log(products)
+}
+
+
+function remove_from_cart(id) {
+    var products = JSON.parse(localStorage.getItem("products"))
+    for (let i=0;i<products.length;i++) {
+        if (id === products[i].id) {
+            products.splice(i, 1)
+            localStorage.setItem("products", JSON.stringify(products))
+            break
+        }
+    }
+    console.log(products)
 }
 
 
