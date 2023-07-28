af8c0c12bb1a50e7ab68d50217188cd38b9911e2
ShailMurtaza <shailmurtaza@gmail.com>
2023-07-28 10:15:05 +0500

    cookies: Created section of cookies which will ask you to accept or deny cookies. Techinacally it does not do anything

---
+++ project/static/js/cookies.js
@@ -0,0 +1,34 @@
+const overlay = get("overlay")
+const cookies = get("cookies")
+
+
+function check_cookies() {
+    let cookies = localStorage.getItem("cookies")
+    if (!cookies) show_cookies()
+}
+
+
+function show_cookies() {
+    overlay.style.display = "block"
+    cookies.style.display = "block"
+    setTimeout(()=>{
+        overlay.style.opacity = 1
+        cookies.style.bottom = "1px"
+    }, 500)
+}
+
+
+function hide_cookies() {
+    localStorage.setItem("cookies", "true")
+
+    overlay.style.opacity = 0
+    cookies.style.bottom = "-400px"
+
+    setTimeout(()=>{
+        overlay.style.display = "none"
+        cookies.style.display = "none"
+    }, 500)
+}
+
+
+setTimeout(check_cookies, 3000)
