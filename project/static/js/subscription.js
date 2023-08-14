4000b6ab10dcc29b9e98bf8faf60990abaefe672
ShailMurtaza <shailmurtaza@gmail.com>
2023-08-14 09:43:32 +0500

    subscription: Add subscription at bottom of page

--- project/static/js/subscription.js
+++ project/static/js/subscription.js
@@ -1,18 +1,8 @@
 const sub = get("sub-container")
 
 function show_sub() {
-    overlay.style.display = "block"
-    setTimeout(()=> {
-        overlay.style.opacity = "1"
-        sub.style.top = "10%"
-    }, 1000)
-
 }
 
 function hide_sub() {
-    sub.style.top = "-100%"
-    overlay.style.opacity = "0"
-    setTimeout(()=>{
-        overlay.style.display = "none"
-    }, 1000)
+    alert("Thanks For Subscribing")
 }
