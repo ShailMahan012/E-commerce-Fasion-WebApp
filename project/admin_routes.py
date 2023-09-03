4a71159b4dbfd8c6ec1f2dbb6c3121d74b71cca8
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-03 11:06:44 +0500

    admin_login: Done admin login and logout

--- project/admin_routes.py
+++ project/admin_routes.py
@@ -3,6 +3,9 @@ from project import app, admin, db
 from project.models import Products, Images, Orders, Cart, Admin
 from project.get_dict import *
 from flask import render_template, request, session, redirect, json, flash
+from werkzeug.utils import secure_filename
+from functools import wraps
+from base64 import b64encode, b64decode
 import os
 from time import time
 
@@ -11,13 +14,24 @@ IMAGE_DIR = app.config['IMAGE_DIR']
 PER_PAGE = app.config['PER_PAGE']
 
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("admin_id") is None:
+            return redirect(f"/admin/login?redirect={b64encode(request.path.encode()).decode()}")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @admin.route("/admin")
+@login_required
 def index():
     return render_template("index.html")
 
 
 @admin.route("/admin/products")
 @admin.route("/admin/products/<int:page>")
+@login_required
 def get_products(page=1):
     products = Products.query.paginate(page=page, per_page=PER_PAGE)
 
@@ -56,6 +70,7 @@ def set_images(product_id, form, primary, secondary):
 
 
 @admin.route("/admin/new_product", methods=["GET", "POST"])
+@login_required
 def new_product():
     if request.method == "POST":
         title = request.form.get("title")
@@ -76,6 +91,7 @@ def new_product():
 
 
 @admin.route("/admin/delete/product/<int:ID>")
+@login_required
 def delete_product(ID):
     product = Products.query.get(ID)
     if product:
@@ -88,6 +104,7 @@ def delete_product(ID):
 
 
 @admin.route("/admin/update/product/<int:ID>", methods=["GET", "POST"])
+@login_required
 def update_product(ID):
     product = Products.query.get(ID)
     images = Images.query.filter_by(product_id=ID)
@@ -119,12 +136,14 @@ def update_product(ID):
 
 @admin.route("/admin/images")
 @admin.route("/admin/images/<int:page>")
+@login_required
 def images(page=1):
     images = Images.query.paginate(page=page, per_page=PER_PAGE)
     return render_template("images.html", images=images)
 
 
 @admin.route("/admin/delete/image/<int:ID>")
+@login_required
 def delete_image(ID):
     image = Images.query.get(ID)
     if image:
@@ -139,6 +158,7 @@ def delete_image(ID):
 
 
 @admin.route("/admin/new_image", methods=["GET", "POST"])
+@login_required
 def new_image():
     if request.method == "POST":
         title = request.form.get("title")
@@ -158,6 +178,7 @@ def new_image():
 
 
 @admin.route("/admin/fetch/images", methods=["POST"])
+@login_required
 def admin_images_fetch():
     search = request.form.get("search")
     images = Images.query.filter(Images.title.like(f"%{search}%"), Images.product_id==None).all()
@@ -167,6 +188,7 @@ def admin_images_fetch():
 
 @admin.route("/admin/orders")
 @admin.route("/admin/orders/<int:page>")
+@login_required
 def orders(page=1):
     orders = Orders.query.paginate(page=page, per_page=PER_PAGE)
     orders_dict = get_orders_dict(orders.items)
@@ -190,6 +212,7 @@ def orders(page=1):
 
 
 @admin.route("/admin/order/mark/<int:ID>/<int:page>")
+@login_required
 def mark_order(ID, page):
     order = Orders.query.get(ID)
     if order:
@@ -201,15 +224,25 @@ def mark_order(ID, page):
 @admin.route("/admin/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        redirect_path = request.args.get("redirect")
+        if not redirect_path:
+            redirect_path = "/admin"
+        else:
+            redirect_path = b64decode(redirect_path).decode()
         uname = request.form["uname"]
         password = request.form["password"]
         user = Admin.query.filter_by(username=uname).first()
         if user and user.verify(password):
-            return redirect("/admin")
+            session['admin_id'] = user.id
+            session['admin_uname'] = user.username
+            return redirect(redirect_path)
         flash("Incorrect information", "danger")
     return render_template("login.html")
 
 
-@app.route("/logout")
+@admin.route("/admin/logout")
+@login_required
 def logout():
-    return redirect("/admin")
+    session.pop('admin_id')
+    session.pop('admin_uname')
+    return redirect("/admin/login")
