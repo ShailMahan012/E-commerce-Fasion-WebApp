6efb585c5f7186817fdea664e7b239aff377ba7b
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-16 15:32:14 +0500

    Subscription: Show thanks section after subscription

--- project/static/js/subscription.js
+++ project/static/js/subscription.js
@@ -1,19 +1,34 @@
 const sub = get("sub-container")
 const sub_email = get("sub-email")
+const sub_thanks = get("sub-thanks")
 
 function subscribe() {
     let email = sub_email.value
     if (email && sub_email.checkValidity()) {
         fetch(`/subscribe?email=${email}`).then(resp => { return resp.text() }).then(resp => {
-            subscription_done()
+            console.log("SUBSCRIPTION: " + resp)
+        }).catch((err)=> {
+            console.log(err)
         })
+        subscription_done()
     }
     else {
-        console.log("False")
+        console.log("Email Validity Error")
     }
 }
 
 
 function subscription_done() {
+    sub_thanks.style.display = "block"
+    setTimeout(()=> {
+        sub_thanks.style.top = "0"
+    }, 100)
+    sub_email.value = ""
+}
 
+function sub_thanks_close() {
+    sub_thanks.style.top = "-100%";
+    sub_thanks.addEventListener("transitionend", ()=> {
+        sub_thanks.style.display = "none"
+    }, {once: true})
 }
