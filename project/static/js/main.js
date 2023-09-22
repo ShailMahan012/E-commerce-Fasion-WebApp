82778684a57b5554d741b86ca8458d50a18c3203
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-22 17:19:51 +0500

    Header: Users: added cart-indicator in users layout

--- project/static/js/main.js
+++ project/static/js/main.js
@@ -1,4 +1,4 @@
-const msg_div = document.getElementById("msg_div")
+const msg_div = get("msg_div")
 
 // Show message with message and alert type like danger, primary and warning etc
 function msg(m, type) {
@@ -21,7 +21,7 @@ function get_products() {
 }
 
 function cart_indicator_update() {
-    const cart_indicator = document.getElementById("cart-indicator")
+    const cart_indicator = get("cart-indicator")
     let products = get_products()
     if (products == null) products = []
     cart_indicator.innerHTML = products.length
