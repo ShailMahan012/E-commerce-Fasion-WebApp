0799636cd7e7be0ad069cd53cb2b5415c4bbe43b
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-26 00:50:38 +0500

    Layout: show msg when item is removed or added to cart

---
+++ project/static/js/msg_home.js
@@ -0,0 +1,14 @@
+const msg_div = get("msg_div")
+
+// Show message with message and alert type like danger, primary and warning etc
+function msg(m, type) {
+    msg_div.innerHTML = m
+    msg_div.className = "alert-home alert alert-" + type
+    msg_div.style.right = "30px"
+}
+
+
+// Just hide msg div
+function hide_msg() {
+    msg_div.style.right = "-100%"
+}
