a82fc8c2812877cce88845e097d929b6244b0e93
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-26 17:50:09 +0500

    checkout: Show thank you message

--- project/static/js/paypal.js
+++ project/static/js/paypal.js
@@ -22,13 +22,16 @@ paypal.Buttons({
       .then((response) => response.json())
       .then((orderData) => {
             localStorage.setItem("products", '[]') // Empty cart after checkout
+            const invoice_id = orderData.purchase_units[0].payments.captures[0].invoice_id
             const element = document.getElementById('form');
-            element.innerHTML = `<div class="row"><h1 class="title">Thanks for payment</h1></div>`
+            element.innerHTML = `<div class="row"><h1 class="title">Thanks for your order</h1></div><div class="row"><h3>INVOICE: ${invoice_id}</h3></div>`
+            hide_paypal()
 
         // Successful capture! For dev/demo purposes:
         console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-        const transaction = orderData.purchase_units[0].payments.captures[0];
-        alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+        msg("Transaction Completed", "primary")
+        // const transaction = orderData.purchase_units[0].payments.captures[0];
+        // alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
         // When ready to go live, remove the alert and show a success message within this page. For example:
         // Or go to another URL:  window.location.href = 'thank_you.html';
       });
