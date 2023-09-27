901c3ff373d71e777793f689633675b5aa287a74
ShailMurtaza <shailmurtaza@gmail.com>
2023-09-27 11:37:31 +0500

    Grabalty PayPal Live

--- project/__init__.py
+++ project/__init__.py
@@ -20,14 +20,17 @@ app.app_context().push()
 
 
 app.config['paypal'] = {
-    # "BASE_URL": "https://api-m.paypal.com",
-    # "CLIENT_ID": "AV6qiggsr5K-Bf-ZtuFdyaCk_cQC3mvEh0LUwAqZXcGvY_WhGp1OxLplyK6Hk61V4womA9yaXFiysuIz",
-    # "APP_SECRET": "EGt1FVt_xxFJXI7G0OmF3SGCXVgyhjnOImt5SzhYw421BeCJnb5tZ41ZKecZZU8SMRHK3cMPUmtVgDoV",
-    "BASE_URL": "https://api-m.sandbox.paypal.com",
+    "BASE_URL": "https://api-m.paypal.com",
+    # Live Grabalty
+    "CLIENT_ID": "AV6qiggsr5K-Bf-ZtuFdyaCk_cQC3mvEh0LUwAqZXcGvY_WhGp1OxLplyK6Hk61V4womA9yaXFiysuIz",
+    "APP_SECRET": "EGt1FVt_xxFJXI7G0OmF3SGCXVgyhjnOImt5SzhYw421BeCJnb5tZ41ZKecZZU8SMRHK3cMPUmtVgDoV",
+    # "BASE_URL": "https://api-m.sandbox.paypal.com",
+    # My sanbox
     # "CLIENT_ID": "ATXfygBnUHBmBMPTjmfwlhgqVqtVoSgov3d5eStEpAi1Vxq_ZSVNFyhE-PvsdIUIesvpbOQOiJudzd-B",
     # "APP_SECRET": "EIh57RP1iOqkLQ5jc8SK6mq2uo1ZPB3Ow7_TSoRm_xxGpP7qFjLDKtHO3qzSVEuRla3oFn9Q3CTdY_KT",
-    "CLIENT_ID": "AaYITUxB_0he79pMHBsHSgs2c6IGRjkbbRJJ5H47mh4GcYbkeiJ60uzXRtgt4-YmmLmiC33yhJ4rrXQm",
-    "APP_SECRET": "EAe0WivXOobHvPhrsVS9ObDaEaaR9pWgPXnRg5UO57m3caAqNWZd_kQf1QvgSDefqW7zurVWYtYZ8Dc-",
+    # Grabalty sandbox
+    # "CLIENT_ID": "AaYITUxB_0he79pMHBsHSgs2c6IGRjkbbRJJ5H47mh4GcYbkeiJ60uzXRtgt4-YmmLmiC33yhJ4rrXQm",
+    # "APP_SECRET": "EAe0WivXOobHvPhrsVS9ObDaEaaR9pWgPXnRg5UO57m3caAqNWZd_kQf1QvgSDefqW7zurVWYtYZ8Dc-",
     "CURRENCY": "CAD"
 }
 app.config['IMAGE_DIR'] =  'project/static/product_images'
